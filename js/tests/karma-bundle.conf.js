9e160b9abf8e40e1924af0f273b7b978ce68993a
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2018-05-24 22:44:30 +0430

    Bootstrap 4.1.1

--- js/tests/karma-bundle.conf.js
+++ js/tests/karma-bundle.conf.js
@@ -1,7 +1,5 @@
 /* eslint-env node */
 /* eslint no-process-env: 0 */
-const path = require('path')
-const jsCoveragePath = path.resolve(__dirname, '../coverage')
 
 module.exports = (config) => {
   const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'assets/js/vendor/jquery-slim.min.js'
@@ -14,19 +12,16 @@ module.exports = (config) => {
       'karma-firefox-launcher',
       'karma-qunit',
       'karma-sinon',
-      'karma-detect-browsers',
-      'karma-coverage-istanbul-reporter'
+      'karma-detect-browsers'
     ],
     // list of files / patterns to load in the browser
     files: [
       jqueryFile,
       'assets/js/vendor/popper.min.js',
-      'js/coverage/dist/util.js',
-      'js/coverage/dist/tooltip.js',
-      'js/coverage/dist/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
+      'dist/js/bootstrap.js',
       'js/tests/unit/*.js'
     ],
-    reporters: ['dots', 'coverage-istanbul'],
+    reporters: ['dots'],
     port: 9876,
     colors: true,
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
@@ -52,19 +47,6 @@ module.exports = (config) => {
         }
 
         throw new Error('Please install Firefox or Chrome')
-      }
-    },
-    coverageIstanbulReporter: {
-      dir: jsCoveragePath,
-      reports: ['lcov', 'text-summary'],
-      thresholds: {
-        emitWarning: false,
-        global: {
-          statements: 90,
-          branches: 84,
-          functions: 87,
-          lines: 90
-        }
       }
     }
   })
